947759bbeefa8f22f4ecd6a86c3268f0da2cfa2b
ashwinlorung <ashwinrainu@gmail.com>
2020-09-24 08:33:10 +0530

    add (Multiple Fetch): add better logs, add allSettled() and any()

--- js/multipleFetch.promise.js
+++ js/multipleFetch.promise.js
@@ -1,36 +1,66 @@
 const getRandomNumber = (upperLimit) => {
   return Math.round(Math.random() * upperLimit);
-}
+};
 
 const mockFetch = (url) => {
   return new Promise((resolve, reject) => {
-    const number = getRandomNumber(100);
+    const number = getRandomNumber(1000);
+    console.log("Request made : " + number);
     setTimeout(() => {
-      console.log("Number: " + number)
-      if(number%2) {
-        reject(`"${url}" rejected: ${number}`);
+      console.log("Response received : " + number);
+      if (number % 10) {
+        resolve(`"${url}" returned: ${number}`);
         return;
       }
-
-      resolve(`"${url}" returned: ${number}`);
+      reject(`"${url}" rejected: ${number}`);
     }, number);
   });
-}
+};
 
-const urls = ["/api/1", "/api/2", "/api/3", "/api/4", "/api/5", "/api/6", "/api/7"];
+const urls = [
+  "/api/1",
+  "/api/2",
+  "/api/3",
+  "/api/4",
+  "/api/5",
+  "/api/6",
+  "/api/7",
+];
 
-Promise.all(urls.map(mockFetch)).then(value => {
-  console.log("All: " + value);
-}).catch(error => {
-  console.log("All: " + error.message);
-})
+Promise.all(urls.map(mockFetch))
+  .then((value) => {
+    console.log("All: " + value);
+  })
+  .catch((error) => {
+    console.log("All: " + error);
+  })
+  .finally(() => console.log("----------------------"));
 
-for(let i = 0; i<1; i++) {
-  setTimeout(() => {
-    Promise.race(urls.map(mockFetch)).then(value => {
-      console.log("Race: " + value);
-    }).catch(error => {
-      console.log("Race: " + error.message);
-    });
-  }, 2000);
-}
+Promise.race(urls.map(mockFetch))
+  .then((value) => {
+    console.log("Race: " + value);
+  })
+  .catch((error) => {
+    console.log("Race: " + error);
+  })
+  .finally(() => console.log("*********************"));
+
+Promise.allSettled(urls.map(mockFetch))
+  .then((value) => {
+    console.log("AllSettled: ");
+    console.log(value);
+  })
+  .catch((error) => {
+    console.log("AllSettled: " + error);
+  })
+  .finally(() => console.log("----------------------"));
+
+// use if(!(number%10)) condition
+Promise.any(urls.map(mockFetch))
+  .then((value) => {
+    console.log("Any: " + value);
+  })
+  .catch((error) => {
+    console.log("Any: " + error);
+  })
+  .finally(() => console.log("----------------------"));
